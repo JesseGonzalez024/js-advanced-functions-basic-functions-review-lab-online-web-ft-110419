860711efc652e6dd719599414377bb86021bb452
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-03-05 17:12:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,9 @@ function wrapAdjective(flare = "*") {
     }
 }
 
-let Calculator
+let Calculator = {
+     add:      (num1 = 0, num2 = 0) => { return (num1 + num2)},
+     subtract: (num1 = 0, num2 = 0) => { return (num1 - num2)},
+     multiply: (num1 = 0, num2 = 0) => { return (num1 * num2)},
+     divide:   (num1 = 0, num2 = 0) => { return (num1 / num2)}
+    }
